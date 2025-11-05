53586a097562ad1d2bac05d961b311b6a31723d9
cheezecakee <anabel.pmoraes@gmail.com>
2025-11-05 15:50:31 -0300

    feat(domain): add muscle entity

---
+++ internal/core/domain/muscle/muscle.go
@@ -0,0 +1,40 @@
+// Package muscle
+package muscle
+
+import (
+	"errors"
+	"strings"
+	"time"
+)
+
+var (
+	ErrEmptyMuscle          = errors.New("empty muscle")
+	ErrInvalidMuscleGroupID = errors.New("invalid muscle group id")
+)
+
+type Muscle struct {
+	ID            *int
+	MuscleGroupID int
+	Name          string
+	CreatedAt     time.Time
+	UpdatedAt     time.Time
+}
+
+func New(name string, muscleGroupID int) (Muscle, error) {
+	if name == "" {
+		return Muscle{}, ErrEmptyMuscle
+	}
+	if muscleGroupID < 0 {
+		return Muscle{}, ErrInvalidMuscleGroupID
+	}
+	return Muscle{
+		Name:          strings.ToLower(name),
+		MuscleGroupID: muscleGroupID,
+		CreatedAt:     time.Now(),
+		UpdatedAt:     time.Now(),
+	}, nil
+}
+
+func (m *Muscle) Touch() {
+	m.UpdatedAt = time.Now()
+}
