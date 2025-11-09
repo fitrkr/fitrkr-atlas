2e78035ac65bfebd4ea7d7fd5bcad1a94c43031b
cheezecakee <anabel.pmoraes@gmail.com>
2025-11-09 11:24:24 -0300

    refactor(domain/equipment): add attachment_type for better separation and testing

---
+++ internal/core/domain/equipment/attachment_type.go
@@ -0,0 +1,37 @@
+package equipment
+
+import "strings"
+
+type AttachmentType int
+
+const (
+	Band AttachmentType = iota + 1
+	Cable
+	Plate
+)
+
+func NewAttachmentType(equipmentType string) (AttachmentType, error) {
+	switch strings.TrimSpace(strings.ToLower(equipmentType)) {
+	case "band":
+		return Band, nil
+	case "cable":
+		return Cable, nil
+	case "weight":
+		return Plate, nil
+	default:
+		return 0, ErrInvalidAttachmentType
+	}
+}
+
+func (a AttachmentType) ToString() string {
+	switch a {
+	case Band:
+		return "band"
+	case Cable:
+		return "cable"
+	case Plate:
+		return "weight"
+	default:
+		return "unknown"
+	}
+}
