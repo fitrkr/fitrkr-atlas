8d96388bac9ca2c14d545966f9b2b05ace1fb3af
cheezecakee <anabel.pmoraes@gmail.com>
2025-11-09 13:45:50 -0300

    refactor(commands/equipment): update delete_attachment to use new read/write

--- internal/core/application/commands/equipments/delete_attachment.go
+++ internal/core/application/commands/equipments/delete_attachment.go
@@ -4,30 +4,24 @@ import (
 	"context"
 	"fmt"
 
-	"github.com/cheezecakee/logr"
-
 	"github.com/cheezecakee/fitrkr-atlas/internal/core/ports"
 )
 
 type DeleteAttachmentCommand struct {
 	ID    int `json:"id"`
-	Write ports.EquipmentAttachmentWrite
+	Write ports.Write
 }
 
 type DeleteAttachmentResp struct{}
 
 func (cmd *DeleteAttachmentCommand) Handle(ctx context.Context) (any, error) {
-	err := cmd.Write.Delete(ctx, cmd.ID)
+	err := cmd.Write.Attachment.Delete(ctx, cmd.ID)
 	if err != nil {
 		if err == ports.ErrAttachmentNotFound {
-			logr.Get().Error("attachment not found")
 			return DeleteAttachmentResp{}, ports.ErrAttachmentNotFound
 		}
-		logr.Get().Errorf("failed to delete attachment: %v", err)
 		return DeleteAttachmentResp{}, fmt.Errorf("failed to delete attachment: %w", err)
 	}
-
-	logr.Get().Info("Attachment deleted successfully")
 
 	return DeleteAttachmentResp{}, nil
 }
