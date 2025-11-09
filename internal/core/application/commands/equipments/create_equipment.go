07a5aa694caf5a82cbdd89479c016d6ce7e1a5ae
cheezecakee <anabel.pmoraes@gmail.com>
2025-11-09 13:45:50 -0300

    refactor(commands/equipment): allow create_equipment to accept attachment options and use new read/write

--- internal/core/application/commands/equipments/create_equipment.go
+++ internal/core/application/commands/equipments/create_equipment.go
@@ -5,32 +5,46 @@ import (
 	"context"
 	"fmt"
 
-	"github.com/cheezecakee/logr"
-
 	"github.com/cheezecakee/fitrkr-atlas/internal/core/domain/equipment"
 	"github.com/cheezecakee/fitrkr-atlas/internal/core/ports"
 )
 
+type EquipmentAttachmentReq struct {
+	AttachmentID int
+}
+
 type CreateEquipmentCommand struct {
 	Name        string `json:"name"`
 	Description string `json:"description"`
-	Write       ports.EquipmentWrite
-	Read        ports.EquipmentRead
+	Type        string `json:"type"`
+	Attachment  []*EquipmentAttachmentReq
+	Write       ports.Write
+	Read        ports.Read
 }
 
 type CreateEquipmentResp struct{}
 
 func (cmd *CreateEquipmentCommand) Handle(ctx context.Context) (any, error) {
-	eq, err := equipment.New(cmd.Name, cmd.Description)
+	equipmentType, err := equipment.NewEquipmentType(cmd.Type)
 	if err != nil {
-		logr.Get().Errorf("failed to create new equipment: %v", err)
+		return nil, fmt.Errorf("failed to create a new equipment type: %w", err)
+	}
+
+	eq, err := equipment.New(cmd.Name, cmd.Description, equipmentType)
+	if err != nil {
 		return CreateEquipmentResp{}, fmt.Errorf("failed to create new equipment: %w", err)
 	}
 
-	err = cmd.Write.Add(ctx, eq)
+	id, err := cmd.Write.Equipment.Add(ctx, eq)
 	if err != nil {
-		logr.Get().Errorf("failed to add equipment to db: %v", err)
 		return CreateEquipmentResp{}, fmt.Errorf("failed to add equipment to db: %w", err)
+	}
+
+	if cmd.Attachment != nil {
+		for _, a := range cmd.Attachment {
+			equipmentAttachment := equipment.NewEquipmentAttachment(id, a.AttachmentID)
+			cmd.Write.Equipment.Attachment.Add(ctx, equipmentAttachment)
+		}
 	}
 
 	return CreateEquipmentResp{}, nil
