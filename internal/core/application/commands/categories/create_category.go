88a38a620f33883c69b5f800fb437f0bf4efabf5
cheezecakee <anabel.pmoraes@gmail.com>
2025-11-12 20:44:09 -0300

    refactor(core): refactor category commands and remove subcategory logic

--- internal/core/application/commands/categories/create_category.go
+++ internal/core/application/commands/categories/create_category.go
@@ -11,25 +11,26 @@ import (
 
 type CreateCategoryCommand struct {
 	Name  string `json:"name"`
+	Type  string `json:"type"`
 	Write ports.Write
 }
 
 type CreateCategoryResp struct{}
 
 func (cmd *CreateCategoryCommand) Handle(ctx context.Context) (any, error) {
-	name, err := category.NewCategoryType(cmd.Name)
+	categoryType, err := category.NewCategoryType(cmd.Type)
 	if err != nil {
-		return CreateCategoryResp{}, fmt.Errorf("failed to create new category type: %w", err)
+		return CreateCategoryResp{}, err
 	}
 
-	c, err := category.New(name)
+	c, err := category.New(cmd.Name, categoryType)
 	if err != nil {
 		return CreateCategoryResp{}, fmt.Errorf("failed to create new category: %w", err)
 	}
 
 	err = cmd.Write.Category.Add(ctx, c)
 	if err != nil {
-		return CreateCategoryResp{}, fmt.Errorf("failed to add category: %w", err)
+		return CreateCategoryResp{}, fmt.Errorf("failed to insert category: %w", err)
 	}
 
 	return CreateCategoryResp{}, nil
--- internal/core/application/commands/categories/update_category.go
+++ internal/core/application/commands/categories/update_category.go
@@ -3,6 +3,7 @@ package categories
 import (
 	"context"
 	"fmt"
+	"strings"
 
 	"github.com/cheezecakee/fitrkr-atlas/internal/core/domain/category"
 	"github.com/cheezecakee/fitrkr-atlas/internal/core/ports"
@@ -11,6 +12,7 @@ import (
 type UpdateCategoryCommand struct {
 	ID    int    `json:"id"`
 	Name  string `json:"name"`
+	Type  string `json:"type"`
 	Write ports.Write
 	Read  ports.Read
 }
@@ -20,15 +22,21 @@ type UpdateCategoryResp struct{}
 func (cmd *UpdateCategoryCommand) Handle(ctx context.Context) (any, error) {
 	existing, err := cmd.Read.Category.GetByID(ctx, cmd.ID)
 	if err != nil {
-		return UpdateCategoryResp{}, fmt.Errorf("failed to get category: %w", err)
+		return UpdateCategoryResp{}, fmt.Errorf("failed to read category: %w", err)
 	}
 
 	if cmd.Name != "" {
-		name, err := category.NewCategoryType(cmd.Name)
-		if err != nil {
-			return UpdateCategoryResp{}, fmt.Errorf("failed to create new category type: %w", err)
-		}
+		name := strings.TrimSpace(strings.ToLower(cmd.Name))
 		existing.Name = name
+	}
+
+	if cmd.Type != "" && cmd.Type != existing.Type.ToString() {
+		categoryType, err := category.NewCategoryType(cmd.Type)
+		if err != nil {
+			return CreateCategoryResp{}, err
+		}
+
+		existing.Type = categoryType
 	}
 
 	existing.Touch()
