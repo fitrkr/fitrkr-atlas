5ac807c3d4e5674509b59ec70665170bb4f8ba03
cheezecakee <anabel.pmoraes@gmail.com>
2025-11-12 20:43:53 -0300

    refactor(core): refactor muscle domain structure and cleanup old types

--- internal/core/domain/muscle/group.go
+++ internal/core/domain/muscle/group.go
@@ -2,31 +2,69 @@ package muscle
 
 import (
 	"errors"
-	"time"
+	"strings"
 )
 
 var (
-	ErrEmptyMuscleGroup   = errors.New("empty muscle group")
-	ErrInvalidMuscleGroup = errors.New("invalid muscle group")
+	ErrInvalidMuscleGroupType = errors.New("invalid muscle group type")
+	ErrEmptyMuscleGroup       = errors.New("empty muscle group")
 )
 
-type Group struct {
-	ID          *int
-	Name        MuscleGroup
-	Description *string
-	CreatedAt   time.Time
-	UpdatedAt   time.Time
+type MuscleGroup int
+
+const (
+	CHEST MuscleGroup = iota + 1
+	BACK
+	SHOULDERS
+	ARMS
+	LEGS
+	CORE
+	NECK
+)
+
+func NewMuscleGroupType(groupType string) (MuscleGroup, error) {
+	if groupType == "" {
+		return 0, ErrEmptyMuscleGroup
+	}
+
+	groupType = strings.TrimSpace(strings.ToLower(groupType))
+	switch groupType {
+	case "chest":
+		return CHEST, nil
+	case "back":
+		return BACK, nil
+	case "shoulders":
+		return SHOULDERS, nil
+	case "arms":
+		return ARMS, nil
+	case "legs":
+		return LEGS, nil
+	case "core":
+		return CORE, nil
+	case "neck":
+		return NECK, nil
+	default:
+		return 0, ErrInvalidMuscleGroupType
+	}
 }
 
-func NewGroup(name MuscleGroup, description string) (Group, error) {
-	return Group{
-		Name:        name,
-		Description: &description,
-		CreatedAt:   time.Now(),
-		UpdatedAt:   time.Now(),
-	}, nil
-}
-
-func (m *Group) Touch() {
-	m.UpdatedAt = time.Now()
+func (m MuscleGroup) ToString() string {
+	switch m {
+	case CHEST:
+		return ""
+	case BACK:
+		return "back"
+	case SHOULDERS:
+		return "shoulders"
+	case ARMS:
+		return "arms"
+	case LEGS:
+		return "legs"
+	case CORE:
+		return "core"
+	case NECK:
+		return "neck"
+	default:
+		return ""
+	}
 }
