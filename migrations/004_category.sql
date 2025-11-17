82149dcd29e80b97abfec1827ea8e3b29be9a600
cheezecakee <anabel.pmoraes@gmail.com>
2025-11-17 12:01:30 -0300

    refactor(db): update category migration to single table approach

--- migrations/004_category.sql
+++ migrations/004_category.sql
@@ -1,21 +1,19 @@
 -- +goose Up
 
+CREATE TYPE category_type AS ENUM (
+    'strength', 
+    'cardio', 
+    'flexibility'
+);
+
 CREATE TABLE category (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL UNIQUE ,
+    type category_type NOT NULL,
     created_at TIMESTAMP, 
     updated_at TIMESTAMP 
 );
 
-CREATE TABLE subcategory (
-    id SERIAL PRIMARY KEY,
-    category_id INT NOT NULL REFERENCES category(id) ON DELETE RESTRICT,
-    name VARCHAR(255) NOT NULL,
-    created_at TIMESTAMP, 
-    updated_at TIMESTAMP, 
-    UNIQUE(name, category_id)
-);
-
 -- +goose Down
-DROP TABLE IF EXISTS subcategory;
 DROP TABLE IF EXISTS category;
+DROP TYPE IF EXISTS category_type;
