6d84616e3c4126a91d07e2c4009b626337d9c428
cheezecakee <anabel.pmoraes@gmail.com>
2025-11-17 12:01:41 -0300

    perf(db): adjust indexes to support updated joined tables

--- migrations/007_index.sql
+++ migrations/007_index.sql
@@ -11,15 +11,11 @@ CREATE INDEX idx_exercise_muscle_muscle_id ON exercise_muscle(muscle_id);
 CREATE INDEX idx_exercise_equipment_id ON exercise_equipment(id);
 CREATE INDEX idx_exercise_equipment_equipment_id ON exercise_equipment(equipment_id);
 
+CREATE INDEX idx_exercise_attachment_id ON exercise_attachment(id);
+CREATE INDEX idx_exercise_attachment_attachment_id ON exercise_attachment(attachment_id);
+
 CREATE INDEX idx_exercise_category_exercise_id ON exercise_category(exercise_id);
-CREATE INDEX idx_exercise_category_subcategory_id ON exercise_category(subcategory_id);
-
--- Relationships (variations, alternatives)
-CREATE INDEX idx_exercise_variation ON exercise_variation(exercise_id);
-CREATE INDEX idx_exercise_variation_variation ON exercise_variation(variation_exercise_id);
-
-CREATE INDEX idx_exercise_alternative ON exercise_alternative(exercise_id);
-CREATE INDEX idx_exercise_alternative_alt ON exercise_alternative(alternate_exercise_id);
+CREATE INDEX idx_exercise_category_category_id ON exercise_category(category_id);
 
 -- Soft deletes
 CREATE INDEX idx_exercise_deleted ON exercise(deleted_at);
@@ -32,10 +28,8 @@ DROP INDEX IF EXISTS idx_exercise_muscle_exercise_id;
 DROP INDEX IF EXISTS idx_exercise_muscle_muscle_id;
 DROP INDEX IF EXISTS idx_exercise_equipment_id;
 DROP INDEX IF EXISTS idx_exercise_equipment_equipment_id;
+DROP INDEX IF EXISTS idx_exercise_attachment_id;
+DROP INDEX IF EXISTS idx_exercise_attachment_attachment_id;
 DROP INDEX IF EXISTS idx_exercise_category_exercise_id;
-DROP INDEX IF EXISTS idx_exercise_category_subcategory_id;
-DROP INDEX IF EXISTS idx_exercise_variation;
-DROP INDEX IF EXISTS idx_exercise_variation_variation;
-DROP INDEX IF EXISTS idx_exercise_alternative;
-DROP INDEX IF EXISTS idx_exercise_alternative_alt;
+DROP INDEX IF EXISTS idx_exercise_category_category_id;
 DROP INDEX IF EXISTS idx_exercise_deleted;
