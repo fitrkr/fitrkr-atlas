f707aba8b92cebc4fcf777ebb31f14e6a15da53b
cheezecakee <anabel.pmoraes@gmail.com>
2025-11-17 12:01:38 -0300

    feat(db): create simplified denormalized exercise view with new schema

--- migrations/006_exercise_read.sql
+++ migrations/006_exercise_read.sql
@@ -1,2 +1,22 @@
 -- +goose Up
+
+CREATE TABLE exercise_view (
+    id INT PRIMARY KEY REFERENCES exercise(id) ON DELETE CASCADE,
+    name VARCHAR(255) NOT NULL UNIQUE,
+    description TEXT,
+    difficulty VARCHAR(50) NOT NULL,
+    position VARCHAR(50) NOT NULL,
+    alias TEXT[],
+    equipment JSONB,
+    muscle JSONB,
+    category JSONB,
+    created_at TIMESTAMP, 
+    updated_at TIMESTAMP, 
+    deleted_at TIMESTAMP, -- Soft delete 
+    purge_at TIMESTAMP -- Auto-delete after 45 days of soft delete
+);
+
 -- +goose Down
+DROP TABLE IF EXISTS exercise_view;
+
+
