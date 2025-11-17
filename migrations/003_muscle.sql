d8c1e22fc67eb1d1f079f86f27c67458997e76f8
cheezecakee <anabel.pmoraes@gmail.com>
2025-11-17 12:01:28 -0300

    refactor(db): update muscle migration for new normalized model

--- migrations/003_muscle.sql
+++ migrations/003_muscle.sql
@@ -1,22 +1,25 @@
 -- +goose Up
 
-CREATE TABLE muscle_group (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL UNIQUE,
-    description TEXT,
-    created_at TIMESTAMP, 
-    updated_at TIMESTAMP 
+CREATE TYPE muscle_group_type AS ENUM (
+    'chest', 
+    'back', 
+    'shoulders', 
+    'arms',
+    'legs',
+    'core',
+    'neck'
 );
 
 CREATE TABLE muscle (
     id SERIAL PRIMARY KEY,
-    muscle_group_id INT NOT NULL REFERENCES muscle_group(id) ON DELETE RESTRICT,
-    name VARCHAR(255) NOT NULL,
-    created_at TIMESTAMP, 
-    updated_at TIMESTAMP, 
-    UNIQUE(name, muscle_group_id)
+    name VARCHAR(255) NOT NULL UNIQUE,
+    muscle_group muscle_group_type NOT NULL,
+    created_at TIMESTAMP,
+    updated_at TIMESTAMP
 );
 
 -- +goose Down
 DROP TABLE IF EXISTS muscle;
-DROP TABLE IF EXISTS muscle_group;
+DROP TYPE IF EXISTS muscle_group_type;
+
+
