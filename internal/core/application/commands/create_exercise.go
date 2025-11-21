7eaed2518b24d886655cae767af3273f553c598e
cheezecakee <anabel.pmoraes@gmail.com>
2025-11-21 12:03:11 -0300

    refactor(app): update exercise create/update commands to use builder

--- internal/core/application/commands/create_exercise.go
+++ internal/core/application/commands/create_exercise.go
@@ -9,13 +9,25 @@ import (
 	"github.com/cheezecakee/fitrkr-atlas/internal/core/ports"
 )
 
+type CreateAlias struct {
+	Add []exercise.Alias `json:"add,omitempty"`
+}
+
+type CreateMuscle struct {
+	Add []MuscleReq `json:"add,omitempty"`
+}
+
 type CreateExerciseCommand struct {
-	Name        string `json:"name"`
-	Description string `json:"description"`
-	Difficulty  string `json:"difficulty"`
-	Position    string `json:"position"`
-	Type        string `json:"type"`
-	EquipmentID *int   `json:"equipment_id,omitempty"`
+	Name        string        `json:"name"`
+	Description string        `json:"description"`
+	Difficulty  string        `json:"difficulty"`
+	Position    string        `json:"position"`
+	Type        string        `json:"type"`
+	EquipmentID *int          `json:"equipment_id,omitempty"`
+	Aliases     *CreateAlias  `json:"aliases,omitempty"`
+	Muscles     *CreateMuscle `json:"muscles,omitempty"`
+	Categories  []int         `json:"categories,omitempty"`
+	Attachments []int         `json:"attachments,omitempty"`
 }
 
 type CreateExerciseResp struct {
@@ -26,6 +38,11 @@ func (cmd *CreateExerciseCommand) Handle(ctx context.Context) (any, error) {
 	// Check if exercise already exists
 	name := strings.TrimSpace(strings.ToLower(cmd.Name))
 	if _, err := read.Exercise.GetByName(ctx, name); err == nil {
+		return nil, ports.ErrDuplicateExercise
+	}
+
+	// Check if alias exists with the same name
+	if _, err := read.Exercise.Alias.GetByName(ctx, name); err == nil {
 		return nil, ports.ErrDuplicateExercise
 	}
 
@@ -47,12 +64,6 @@ func (cmd *CreateExerciseCommand) Handle(ctx context.Context) (any, error) {
 		return CreateExerciseResp{}, fmt.Errorf("failed to create new exercise: %w", err)
 	}
 
-	// Add exercise to DB and get exercise back
-	ex, err = write.Exercise.Add(ctx, ex)
-	if err != nil {
-		return CreateExerciseResp{}, fmt.Errorf("failed to insert exercise: %w", err)
-	}
-
 	// Validate equipment if provided
 	if cmd.EquipmentID != nil {
 		equipment, err := read.Equipment.GetByID(ctx, *cmd.EquipmentID)
@@ -62,7 +73,47 @@ func (cmd *CreateExerciseCommand) Handle(ctx context.Context) (any, error) {
 		ex.EquipmentID = equipment.ID
 	}
 
+	// Add exercise to DB and get exercise back
+	ex, err = write.Exercise.Add(ctx, ex)
+	if err != nil {
+		return CreateExerciseResp{}, fmt.Errorf("failed to insert exercise: %w", err)
+	}
+
+	if err = cmd.build(ctx, *ex.ID); err != nil {
+		return CreateExerciseResp{}, fmt.Errorf("failed to create exercise relations: %w", err)
+	}
+
 	return CreateExerciseResp{ID: *ex.ID}, nil
+}
+
+func (cmd *CreateExerciseCommand) build(ctx context.Context, exerciseID int) error {
+	builder := NewExerciseBuilder(ctx, exerciseID)
+
+	if cmd.EquipmentID != nil {
+		builder.equipmentID = cmd.EquipmentID
+	}
+
+	if cmd.Aliases != nil {
+		builder.WithAlias(cmd.Aliases.Add, nil)
+	}
+
+	if cmd.Muscles != nil {
+		builder.WithMuscles(cmd.Muscles.Add, nil)
+	}
+
+	if cmd.Categories != nil {
+		builder.WithCategories(cmd.Categories, nil)
+	}
+
+	if cmd.Attachments != nil {
+		builder.WithAttachments(cmd.Attachments, nil)
+	}
+
+	if err := builder.Execute(); err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func init() {
--- internal/core/application/commands/update_exercise.go
+++ internal/core/application/commands/update_exercise.go
@@ -9,13 +9,37 @@ import (
 	"github.com/cheezecakee/fitrkr-atlas/internal/core/ports"
 )
 
+type UpdateAlias struct {
+	Add    []exercise.Alias `json:"add,omitempty"`
+	Remove []int            `json:"remove,omitempty"`
+}
+
+type UpdateMuscle struct {
+	Add    []MuscleReq `json:"add,omitempty"`
+	Remove []int       `json:"remove,omitempty"`
+}
+
+type UpdateCategory struct {
+	Add    []int `json:"add,omitempty"`
+	Remove []int `json:"remove,omitempty"`
+}
+
+type UpdateAttachment struct {
+	Add    []int `json:"add,omitempty"`
+	Remove []int `json:"remove,omitempty"`
+}
+
 type UpdateExerciseCommand struct {
-	ID          int    `json:"id"`
-	Name        string `json:"name"`
-	Description string `json:"description"`
-	Difficulty  string `json:"difficulty"`
-	Position    string `json:"position"`
-	EquipmentID *int   `json:"equipment_id,omitempty"`
+	ID          int               `json:"id"`
+	Name        string            `json:"name"`
+	Description string            `json:"description"`
+	Difficulty  string            `json:"difficulty"`
+	Position    string            `json:"position"`
+	EquipmentID *int              `json:"equipment_id,omitempty"`
+	Aliases     *UpdateAlias      `json:"aliases,omitempty"`
+	Muscles     *UpdateMuscle     `json:"muscles,omitempty"`
+	Categories  *UpdateCategory   `json:"categories,omitempty"`
+	Attachments *UpdateAttachment `json:"attachments,omitempty"`
 }
 
 type UpdateExerciseResp struct {
@@ -28,14 +52,21 @@ func (cmd *UpdateExerciseCommand) Handle(ctx context.Context) (any, error) {
 		return nil, ports.ErrExerciseNotFound
 	}
 
+	var name string
+
 	if cmd.Name != "" {
-		name := strings.ToLower(cmd.Name)
-		exists.Name = strings.TrimSpace(name)
+		name = strings.ToLower(strings.TrimSpace(cmd.Name))
+		exists.Name = name
+	}
+
+	// Check if alias exists with the same name
+	if _, err := read.Exercise.Alias.GetByName(ctx, name); err == nil {
+		return nil, ports.ErrDuplicateExercise
 	}
 
 	if cmd.Description != "" {
 		description := strings.ToLower(cmd.Description)
-		exists.Name = strings.TrimSpace(description)
+		exists.Description = &description
 	}
 
 	if cmd.Difficulty != "" {
@@ -70,7 +101,41 @@ func (cmd *UpdateExerciseCommand) Handle(ctx context.Context) (any, error) {
 		return nil, fmt.Errorf("failed to update exercise: %w", err)
 	}
 
+	if err = cmd.build(ctx); err != nil {
+		return UpdateExerciseResp{}, fmt.Errorf("failed to update exercise relations: %w", err)
+	}
+
 	return UpdateExerciseResp{}, nil
+}
+
+func (cmd *UpdateExerciseCommand) build(ctx context.Context) error {
+	builder := NewExerciseBuilder(ctx, cmd.ID)
+
+	if cmd.EquipmentID != nil {
+		builder.equipmentID = cmd.EquipmentID
+	}
+
+	if cmd.Aliases != nil {
+		builder.WithAlias(cmd.Aliases.Add, cmd.Aliases.Remove)
+	}
+
+	if cmd.Muscles != nil {
+		builder.WithMuscles(cmd.Muscles.Add, cmd.Muscles.Remove)
+	}
+
+	if cmd.Categories != nil {
+		builder.WithCategories(cmd.Categories.Add, cmd.Categories.Remove)
+	}
+
+	if cmd.Attachments != nil {
+		builder.WithAttachments(cmd.Attachments.Add, cmd.Attachments.Remove)
+	}
+
+	if err := builder.Execute(); err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func init() {
